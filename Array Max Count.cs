49483220451cd91ac7765c4168f99b39c68cfa22
Shahzaib Hassan <61833621+Shahzaib096@users.noreply.github.com>
2020-03-10 18:54:16 +0500

    Add files via upload

---
+++ Array Max Count.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Max_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Size = 5;
+            int[] myArray = new int[5] { 1, 2, 3, 4, 4 };
+            Array.Sort(myArray);
+            int Count = 0;
+            for (int i = 0; i < Size; i++)
+            {
+                if (myArray[i] == myArray[Size - 1])
+                {
+                    Count++;
+                }
+            }
+            Console.WriteLine("Array Max Values Count is :"+Count);
+            Console.Read();
+        }
+    }
+}
